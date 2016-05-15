1268fb6b21fb5cd8155e0f39b0f727202658c979
danprince <danielprince@hotmail.co.uk>
2016-05-15 13:36:27 +0100

    hotfix for broken tokens

--- src/tokens.js
+++ src/tokens.js
@@ -1,7 +1,7 @@
 module.exports = [
   { name: 'string',  pattern: /^"[^"]*?"/ },
   { name: 'number',  pattern: /^\d+/ },
-  { name: 'comment', pattern: /^--.*?\n?$/ },
+  { name: 'comment', pattern: /^--.*(\n|$)/ },
   { name: 'open',    pattern: /^\(/ },
   { name: 'close',   pattern: /^\)/ },
   { name: 'symbol',  pattern: /^[^\s\(\)"]+/ }
