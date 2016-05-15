97e4cc7c0a18869b07de93b87d9bc88f18166c4f
danprince <danielprince@hotmail.co.uk>
2016-05-15 19:36:19 +0100

    adds test stubs for traverser tests

--- tests/traverser.spec.js
+++ tests/traverser.spec.js
@@ -1,5 +1,6 @@
 const test = require('tape');
 
+const _ = require('../src/util');
 const lexer = require('../src/lexer');
 const parser = require('../src/parser');
 const traverse = require('../src/traverse');
@@ -7,6 +8,19 @@ const traverse = require('../src/traverse');
 const toAST = src => traverse(parser(lexer(src)));
 
 test('traverse', t => {
+  // simplifcation step
+  t.skip('should replace term nodes');
+  t.skip('should work recursively with blocks');
+  t.skip('should replace literal nodes');
+  t.skip('should add name prop to symbol nodes');
+  t.skip('should add parse number nodes');
+  t.skip('should add parse string nodes');
+  t.skip('should remove comment nodes');
+  t.skip('should remove block comment nodes');
 
+  // expansion step
+  t.skip('should expand import statements');
+  t.skip('should expand function statements');
+  t.skip('should expand macro statements');
 });
 
